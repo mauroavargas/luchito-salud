5b6744902dafdae06b63bf83db5cbbdb8a408651
mauroavargas <mauro.vargas@catodigo.co>
2026-08-14 12:17:49 -0500

    deploy: 2026-08-14 12:17

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: la app se sirve desde https://<usuario>.github.io/luchito-salud/
-export default defineConfig({
-  plugins: [react()],
-  base: '/luchito-salud/',
-})
